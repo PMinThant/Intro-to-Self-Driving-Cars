f5f7c4225ae9a00f00f90277d3d13645e62eb77b
Phyoe Min Thant <44871524+PMinThant@users.noreply.github.com>
2019-11-19 17:00:00 +0630

    upload C++ Basics Exercises

---
+++ C++ Basics/Fix this Code ans/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int x;
+    float y,z,division;
+
+	x = 25;
+	y = 61.4;
+	z = 199.2;
+
+	division = y / z;
+
+	cout << x << '\n';
+	cout << y << '\n';
+	cout << division << '\n';
+
+	return 0;
+}
